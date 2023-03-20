0808c5b5880d7d0d84f597817b2c411c86e5e248
CREGOEUGENIA <eugeniacrego@gmail.com>
2023-03-20 20:53:11 -0300

    areaRectangulo

--- Cocinar torta.ts
+++
@@ -1,9 +0,0 @@
-console.log("precalentar el horno 180°");
-console.log("enmantecar la fuente y reservar");
-console.log("batir 3 huevos con 3/4 de taza de azucar hasta el punto letra");
-console.log("Añadir !/2 taza de aceite y batir");
-console.log("añadir 1/2 taza de leche y 1 cucharadita de esencia de vainilla y batir");
-console.log("mezclar aparte 1 1/2 taza de harina, 1 1/2 cucharadita de royal y 1/2 cucharadita de sal");
-console.log("incorporar la harina a la mezcla de huevos con movimientos envolventes");
-console.log("hornear por 30 min hasta que se introduzca un cuchillo y salga limpio");
-console.log("dejar enfriar por 15 minutos, sacar del molde y dejar enfriar por completo");
